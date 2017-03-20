81290857290b61380dd4d0eaaf3166b7dedeaa8a
evanmckenna18 <evanmckenna18@marist.com>
2017-03-20 09:41:43 -0400

    Update TradeOrder.java

--- TradeOrder.java
+++ TradeOrder.java
@@ -8,7 +8,42 @@ public class TradeOrder {
   private double price;
   
   public TradeOrder(Trader trader, String symbol,boolean buyOrder, boolean marketOrder, int numShares, double price){
-    
+    this.trader = trader;
+    this.symbol = symbol;
+    this.buyOrder = buyOrder;
+    this.marketOrder = marketOrder;
+    this.numShares = numShares;
+    this.price = price;
+  }
+  
+  public Trader getTrader(){
+    return trader;
+  }
+  public String getSymbol(){
+    return symbol;
+  }
+  public boolean isBuy(){
+    return buyOrder;
+  }
+  public boolean isSell(){
+    return !buyOrder;
+  }
+  public boolean isMarket(){
+    return marketOrder;
+  }
+  public boolean isLimit(){
+    return !marketOrder;
+  }
+  public int getShares(){
+    return numShares;
+  }
+  public void subtractShares(int shares){
+    if(shares > getShares()){
+     throw new IllegalArgumentExecption();
+    }
+    else{
+      numShares = getShares() - shares;
+    }
   }
     
 }
