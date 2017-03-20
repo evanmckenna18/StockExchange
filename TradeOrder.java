3304067f2ee41f6ae96a6a30b96611b93102b480
evanmckenna18 <evanmckenna18@marist.com>
2017-03-20 09:31:33 -0400

    Update TradeOrder.java

--- TradeOrder.java
+++ TradeOrder.java
@@ -2,4 +2,13 @@
  * Created by EvanMcKenna18 on 3/17/2017.
  */
 public class TradeOrder {
+  private String trader, symbol;
+  private boolean buyOrder, marketOrder;
+  private int numShares;
+  private double price;
+  
+  public TradeOrder(Trader trader, String symbol,boolean buyOrder, boolean marketOrder, int numShares, double price){
+    
+  }
+    
 }
