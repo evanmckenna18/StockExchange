62f31e21a4cb94ddaf73a4a2e206c789836cbfe1
MichaudR <rebeccamichaud17@marist.com>
2017-03-20 10:00:43 -0400

    Update StockExchange.java

--- StockExchange.java
+++ StockExchange.java
@@ -2,4 +2,40 @@
  * Created by EvanMcKenna18 on 3/17/2017.
  */
 public class StockExchange {
+  
+  public StockExchange(){
+    
+  }
+  
+  
+  //======================================================================================================================================
+  
+  public String getQuote(String symbol){
+    
+  }
+  
+  
+  //======================================================================================================================================
+  
+  public void listStock(String symbol, String name, double price){
+    
+    //create a HashMap and put newStock on it
+   
+    Map<Stock, String> map = new HashMap<Stock, String>();
+    
+    Stock newStock = new Stock(symbol, name, price);
+    
+    map.put(newStock, symbol);    
+    
+    
+  }
+  
+  //======================================================================================================================================
+  
+  public void placeOrder(TradeOrder order){
+    
+    stock.placeOrder(order);
+    
+  }
+  
 }
