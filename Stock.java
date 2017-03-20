5c77ddc084a575e914f4f7d7bb942662f0ccbd77
evanmckenna18 <evanmckenna18@marist.com>
2017-03-19 21:42:18 -0400

    Update Stock.java

--- Stock.java
+++ Stock.java
@@ -2,4 +2,22 @@
  * Created by EvanMcKenna18 on 3/17/2017.
  */
 public class Stock {
+  public static java.text.DecimalFormat money;
+  private String symbol,name;
+  private double price;
+  
+  public Stock(String symbol, String name, double Price){
+    this.symbol = symbol;
+    this.name = name;
+    this.price = price;
+  }
+  
+  public String getQuote(){
+    
+  }
+  
+  public void placeOrder(Object order){
+    
+  }
+    
 }
