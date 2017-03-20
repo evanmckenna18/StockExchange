381448622264166e69e6e7f51a0f33b6aef7d840
MichaudR <rebeccamichaud17@marist.com>
2017-03-20 09:43:37 -0400

    Update Brokerage.java

--- Brokerage.java
+++ Brokerage.java
@@ -1,5 +1,26 @@
 /**
  * Created by EvanMcKenna18 on 3/17/2017.
  */
-public class Brokerage {
+public class Brokerage implements Login {
+  
+  public Brokerage(StockExchange exchange){
+    
+  }
+  
+  public int addUser(String name, String password){
+    
+  }
+  
+  public void getQuote(String symbol, Trader trader){
+    
+  }
+  
+  public int login(Trader trader){
+    
+  }
+  
+  public void placeOrder(TradeOrder order){
+    
+  }
+  
 }
