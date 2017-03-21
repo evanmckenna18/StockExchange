cad2a90a6bcbdfa19bac3f3d76753ec511471322
evanmckenna18 <evanmckenna18@marist.com>
2017-03-21 08:34:21 -0400

    Update Stock.java

--- Stock.java
+++ Stock.java
@@ -4,19 +4,29 @@
 public class Stock {
   public static java.text.DecimalFormat money;
   private String symbol,name;
-  private double price;
+  private double price, high, low, last;
+  private volume;
+  PriorityQueue buy;
+  PriceComparator
+  PriorityQueue sell;
+  
   
   public Stock(String symbol, String name, double Price){
     this.symbol = symbol;
     this.name = name;
     this.price = price;
+    high = price;
+    low = price;
+    last = price;
+    volume = 0;
+    
   }
   
   public String getQuote(){
     
   }
   
-  public void placeOrder(Object order){
+  public void placeOrder(TradeOrder order){
     
   }
     
