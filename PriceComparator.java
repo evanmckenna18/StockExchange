11fc2c54c2ce0cd7e37f21deff971365892a3c21
tannerhess18 <tannerhess18@marist.com>
2017-03-22 13:20:06 -0400

    Update PriceComparator.java

--- PriceComparator.java
+++ PriceComparator.java
@@ -1,7 +1,14 @@
 /**
  * Created by EvanMcKenna18 on 3/17/2017.
  */
-public class PriceComparator {
+java.util.Comparator<TradeOrder>;
+
+public class PriceComparator implements Comparator<TradeOrder>{
+  PriceComparator(){
+  }
+  
+  PriceComparator(boolean asc){
+  }
   public int compare(TradeOrder order1, TradeOrder order2){
   }
 }
