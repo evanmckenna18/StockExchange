564a080516ee2cfa17bef004426d3511f5015aff
tannerhess18 <tannerhess18@marist.com>
2017-03-22 13:39:20 -0400

    Update PriceComparator.java

--- PriceComparator.java
+++ PriceComparator.java
@@ -15,21 +15,21 @@ public class PriceComparator implements Comparator<TradeOrder>{
   }
   
   public int compare(TradeOrder order1, TradeOrder order2){
-    if ( order1.isMarket() && order2.isMarket()){
+    if (order1.isMarket() && order2.isMarket()){
       return 0;
     }
-    if ( order1.isMarket() && order2.isLimit()){
+    if (order1.isMarket() && order2.isLimit()){
       return -1;
     }
-    if ( order1.isLimit() && order2.isMarket()){
+    if (order1.isLimit() && order2.isMarket()){
       return 1;
     }
     else{
-      if ( ascending == true ){
-        return (int)( ( order1.getPrice() - order2.getPrice() ) * 100 );
+      if (ascending == true){
+        return (int) ((order1.getPrice() - order2.getPrice()) * 100);
       }
       else {
-        return (int)( ( order2.getPrice() - order1.getPrice() ) * 100 );
+        return (int) ((order2.getPrice() - order1.getPrice()) * 100);
       }
     }
   }
