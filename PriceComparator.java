e3c8413a35d0205676605029f5dc17a20b8fcd42
tannerhess18 <tannerhess18@marist.com>
2017-03-22 13:25:38 -0400

    Update PriceComparator.java

--- PriceComparator.java
+++ PriceComparator.java
@@ -4,15 +4,33 @@
 java.util.Comparator<TradeOrder>;
 
 public class PriceComparator implements Comparator<TradeOrder>{
-  private boolean ascendingFlag;
+  private boolean ascending;
   
   PriceComparator(){
-    ascendingFlag = true;
+    ascending = true;
   }
   
   PriceComparator(boolean asc){
-    ascendingFlag = asc;
+    ascending = asc;
   }
+  
   public int compare(TradeOrder order1, TradeOrder order2){
+    if ( order1.isMarket() && order2.isMarket()){
+      return 0;
+    }
+    if ( order1.isMarket() && order2.isLimit()){
+      return -1;
+    }
+    if ( order1.isLimit() && order2.isMarket()){
+      return 1;
+    }
+    else{
+      if ( ascending == true ){
+        return (int)( ( order1.getPrice() - order2.getPrice() ) * 100 );
+      }
+      else {
+        return (int)( ( order2.getPrice() - order1.getPrice() ) * 100 );
+      }
+    }
   }
 }
