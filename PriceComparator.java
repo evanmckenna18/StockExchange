dca672de1a3020e46aee7ca4f39e55887d665456
tannerhess18 <tannerhess18@marist.com>
2017-03-24 15:30:08 -0400

    Update PriceComparator.java

--- PriceComparator.java
+++ PriceComparator.java
@@ -6,14 +6,36 @@ java.util.Comparator<TradeOrder>;
 public class PriceComparator implements Comparator<TradeOrder>{
   private boolean ascending;
   
+  /**
+  * constructs a basic price comparator that compares two order in ascending order
+  **/
   PriceComparator(){
     ascending = true;
   }
   
+  //=============================================================================================================================
+
+  /**
+  * constructs a price comparator that compares two orders in either ascending or descending order based on asc
+  * parameters: asc
+  **/
   PriceComparator(boolean asc){
     ascending = asc;
   }
   
+  //=============================================================================================================================
+  
+  /**
+  *compares two trade orders
+  * parameters: order1- the first order
+  * order2- the second order
+  * returns: 0 if both orders are market orders;
+  * -1 if order1 is market and order2 is limit;
+  * 1 if order1 is limit and order2 is market;
+  * the difference in prices, rounded to the nearest cent, if both order1 and order2 are limit orders. 
+  * In the latter case, the difference returned is cents1 - cents2 or cents2 - cents1, depending on whether this is an ascending or 
+  * descending comparator (ascending is true or false).
+  **/
   public int compare(TradeOrder order1, TradeOrder order2){
     if (order1.isMarket() && order2.isMarket()){
       return 0;
